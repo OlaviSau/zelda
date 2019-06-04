e07a1642dcd2be9e878c2898e7bca552607e4ac8
Olavi Sau <olavi.sau@betssongroup.com>
2019-06-04 15:03:24 +0300

    fix: remove dependency on windows-build-tools

--- builder/src/electron-dev-server/index.ts
+++ builder/src/electron-dev-server/index.ts
@@ -1,12 +1,15 @@
 import { createBuilder } from "@angular-devkit/architect";
 import { DevServerBuilderOptions, executeDevServerBuilder } from "@angular-devkit/build-angular";
+import * as path from "path";
 
 export default createBuilder<DevServerBuilderOptions>((options, context) => executeDevServerBuilder(options, context, {
   webpackConfiguration: (configuration) => ({
     ...configuration,
     ...{
       target: "electron-renderer",
-      externals: (ctx: any, req: any, done: any) => (/^node-pty$/.test(req) ? done(null, `commonjs ${req}`) : done())
+      externals: (ctx: any, req: any, done: any) => (/^node-pty$/.test(req) ? done(
+        null, `commonjs ${path.resolve(context.workspaceRoot, "./node-pty")}`
+      ) : done())
     }
   })
 }));
