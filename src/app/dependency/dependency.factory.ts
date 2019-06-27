b7431f8271fb0c89a1cf76f3d7f4061c0dc17cb4
Olavi Sau <olavi.sau@betssongroup.com>
2019-06-27 22:05:49 +0300

    refactor: remove needless code

--- src/app/dependency/dependency.factory.ts
+++ src/app/dependency/dependency.factory.ts
@@ -1,13 +1,10 @@
 import { Dependency, DependencyType } from "../app.model";
 import { lernaStrategy } from "./strategy/lerna.strategy";
-import { DependencyStrategy } from "./dependency-strategy";
 import { packageStrategy } from "./strategy/package.strategy";
 
-const DependencyTypeToStrategy: {[key in DependencyType]: DependencyStrategy} = {
-  [DependencyType.Lerna]: lernaStrategy,
-  [DependencyType.Package]: packageStrategy
-};
-
 export function dependencyFactory(dependency: Dependency) {
-  return DependencyTypeToStrategy[dependency.type](dependency);
+  return {
+    [DependencyType.Lerna]: lernaStrategy,
+    [DependencyType.Package]: packageStrategy
+  }[dependency.type](dependency);
 }
