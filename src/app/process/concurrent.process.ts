f25d859d99f9c53440c88811665a4071f644e4e8
Olavi Sau <olavi.sau@betssongroup.com>
2019-08-19 11:18:20 +0300

    refactor: remove concurrent process for now

--- src/app/process/concurrent.process.ts
+++ src/app/process/concurrent.process.ts
@@ -1,26 +1,26 @@
-import { ReplaySubject } from "rxjs";
-import { Process } from "./process";
-import { Directory } from "../util/directory";
+// import { ReplaySubject } from "rxjs";
+// import { Process } from "./process";
+// import { Directory } from "../util/directory";
 
-export class ConcurrentProcess implements Process {
-  private completedCount = 0;
-  constructor(private commands: Process[], readonly name?: string) {}
-  readonly buffer$ = new ReplaySubject<string>();
+// export class ConcurrentProcess implements Process {
+//   private completedCount = 0;
+//   constructor(private commands: Process[], readonly name?: string) {}
+//   readonly buffer$ = new ReplaySubject<string>();
 
-  execute(rows: number, args: Directory<string>) {
-    for (const command of this.commands) {
-      command.execute(rows, args);
-      command.buffer$.subscribe({
-        next: chunk => this.buffer$.next(chunk),
-        error: err => this.buffer$.error(err),
-        complete: () => this.completedCount === this.commands.length ? this.buffer$.complete() : null
-      });
-    }
-  }
+//   execute(rows: number, args: Directory<string>) {
+//     for (const command of this.commands) {
+//       command.execute(rows, args);
+//       command.buffer$.subscribe({
+//         next: chunk => this.buffer$.next(chunk),
+//         error: err => this.buffer$.error(err),
+//         complete: () => this.completedCount === this.commands.length ? this.buffer$.complete() : null
+//       });
+//     }
+//   }
 
-  kill() {
-    for (const command of this.commands) {
-      command.kill();
-    }
-  }
-}
+//   kill() {
+//     for (const command of this.commands) {
+//       command.kill();
+//     }
+//   }
+// }
